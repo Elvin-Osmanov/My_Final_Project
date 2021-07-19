9a1374d026b9047f3b99ad68d28d4192d63d54f8
Elvin-Osmanov <elvinfo@code.edu.az>
2021-07-19 01:43:46 +0100

    testimonial done but store section needs development

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+  
+  
+$('.carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots:true
+ 
+});
+
+   console.log($(".slick-list"))
+});
