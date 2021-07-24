00806598041d2a3b59c4643714e83ce98320d53a
Elvin-Osmanov <elvinfo@code.edu.az>
2021-07-24 15:20:51 +0100

    checkout menu sign up pages done

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,29 @@ $('.play-btn').magnificPopup({
 
 
 
+$(".zoom").click(function(){
+
+  $("#menus .context .left img").each(function(){
+    var source=$(this).attr('src');
+  
+    $(this).parent().attr('href' , source)
+    
+   
+  })
+})
+
+
+
+$('.zoom').magnificPopup({
+  disableOn: 700,
+   type: 'image',
+   mainClass: 'mfp-fade',
+   removalDelay: 160,
+   preloader: false,
+   fixedContentPos: true
+});
+
+
 
 $(".back").click(function(e) {
   e.preventDefault();
@@ -152,93 +175,19 @@ $(window).scroll(function () {
   
 });
 
+$("#menus .menu-tabs ul li").each(function(e){
+  
+  $(this).click(function(e){
+    e.preventDefault();
+    
+
+    $('.menu-tabs ul li.active').removeClass("active")
+    $(this).addClass("active")
+
+    var href = $(this).attr('href')
+    
+    $(".menu-boxes div.show").removeClass("show")
+    $(href).addClass("show")
+})
+})
 });
-
-// let map;
-
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("contact-intro"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//     styles: [
-//       { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-//       { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-//       { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-//       {
-//         featureType: "administrative.locality",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#d59563" }],
-//       },
-//       {
-//         featureType: "poi",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#d59563" }],
-//       },
-//       {
-//         featureType: "poi.park",
-//         elementType: "geometry",
-//         stylers: [{ color: "#263c3f" }],
-//       },
-//       {
-//         featureType: "poi.park",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#6b9a76" }],
-//       },
-//       {
-//         featureType: "road",
-//         elementType: "geometry",
-//         stylers: [{ color: "#38414e" }],
-//       },
-//       {
-//         featureType: "road",
-//         elementType: "geometry.stroke",
-//         stylers: [{ color: "#212a37" }],
-//       },
-//       {
-//         featureType: "road",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#9ca5b3" }],
-//       },
-//       {
-//         featureType: "road.highway",
-//         elementType: "geometry",
-//         stylers: [{ color: "#746855" }],
-//       },
-//       {
-//         featureType: "road.highway",
-//         elementType: "geometry.stroke",
-//         stylers: [{ color: "#1f2835" }],
-//       },
-//       {
-//         featureType: "road.highway",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#f3d19c" }],
-//       },
-//       {
-//         featureType: "transit",
-//         elementType: "geometry",
-//         stylers: [{ color: "#2f3948" }],
-//       },
-//       {
-//         featureType: "transit.station",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#d59563" }],
-//       },
-//       {
-//         featureType: "water",
-//         elementType: "geometry",
-//         stylers: [{ color: "#17263c" }],
-//       },
-//       {
-//         featureType: "water",
-//         elementType: "labels.text.fill",
-//         stylers: [{ color: "#515c6d" }],
-//       },
-//       {
-//         featureType: "water",
-//         elementType: "labels.text.stroke",
-//         stylers: [{ color: "#17263c" }],
-//       },
-//     ]
-//   });
-// }
